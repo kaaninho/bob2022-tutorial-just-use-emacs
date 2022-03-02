8e567c2bf4dad0c6c05c419349c78fa7b3510ad2
Kaan Sahin <kaan.sahin@active-group.de>
2022-03-02 15:42:52 +0100

    Add code examples

---
+++ code/python.py
@@ -0,0 +1,7 @@
+# Python Program to convert temperature in celsius to fahrenheit
+
+def celsius_to_fahrenheit (celsius):
+    """Converts Celsius to Fahrenheit"""
+    return (celsius * 1.8) + 32
+
+print(celsius_to_fahrenheit(30))
